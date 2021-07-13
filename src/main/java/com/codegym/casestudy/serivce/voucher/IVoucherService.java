62cc022b47b44d539a8f1c2bc8a2f4a248646a7f
lehoang081191 <lehoang0811@gmail.com>
2021-07-13 22:21:17 +0700

    update

--- src/main/java/com/codegym/casestudy/serivce/voucher/IVoucherService.java
+++ src/main/java/com/codegym/casestudy/serivce/voucher/IVoucherService.java
@@ -4,6 +4,8 @@ import com.codegym.casestudy.model.Product;
 import com.codegym.casestudy.model.Voucher;
 import com.codegym.casestudy.serivce.IGeneralService;
 
+import java.text.ParseException;
+
 public interface IVoucherService extends IGeneralService<Voucher> {
     Iterable<Voucher> findAllByOrderByVoucher_idDesc();
 
@@ -11,5 +13,5 @@ public interface IVoucherService extends IGeneralService<Voucher> {
 
     void restoreVoucherById(Long id);
 
-//    void checkExpired();
+    void checkExpired() throws ParseException;
 }
--- src/main/java/com/codegym/casestudy/serivce/voucher/VoucherService.java
+++ src/main/java/com/codegym/casestudy/serivce/voucher/VoucherService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Optional;
 
 @Service
@@ -17,6 +19,11 @@ public class VoucherService implements IVoucherService {
 
     @Override
     public Iterable<Voucher> findAll() {
+        try {
+            checkExpired();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
         return voucherRepository.findAllByOrderByVoucher_idDesc();
     }
 
@@ -51,21 +58,25 @@ public class VoucherService implements IVoucherService {
         voucherRepository.restoreVoucherById(id);
     }
 
-//    @Override
-//    public void checkExpired() {
-//        Iterable<Voucher> listVoucher = voucherRepository.findAllByOrderByVoucher_idDesc();
-//        for (Voucher v: listVoucher
-//             ) {
-//            if (compareDate(v.getExpiration_date())<=0){
-//                v.setExpired(true);
-//                voucherRepository.save(v);
-//            }
-//        }
-//    }
-//
-//   private int compareDate(Date expDate) {
-//       java.util.Date utilDate = new java.util.Date();
-//       java.sql.Date currentDate = new java.sql.Date(utilDate.getTime());
-//       return expDate.compareTo(currentDate);
-//    }
+    @Override
+    public void checkExpired() throws ParseException {
+        Iterable<Voucher> listVoucher = voucherRepository.findAllByOrderByVoucher_idDesc();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        java.util.Date utilDate = new java.util.Date();
+        java.util.Date currentDate = new java.sql.Date(utilDate.getTime());
+        java.util.Date releaseDay;
+        java.util.Date expDay;
+        for (Voucher v: listVoucher
+        ) {
+            releaseDay = formatter.parse(v.getRelease_date());
+            expDay = formatter.parse(v.getExpiration_date());
+            if (currentDate.getTime()-releaseDay.getTime()>0 && expDay.getTime()-currentDate.getTime()>0){
+                v.setExpired(false);
+                voucherRepository.save(v);
+            }else{
+                v.setExpired(true);
+                voucherRepository.save(v);
+            }
+        }
+    }
 }
